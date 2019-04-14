a00eee45e9f11294b8aa5e71b3afc798b18ec71b
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-04-14 13:40:53 +0300

    no message

--- output/httpResponse.go
+++ output/httpResponse.go
@@ -1,8 +1,8 @@
 package output
 
 import (
-  "fmt"
-  "encoding/json"
+    "fmt"
+    "encoding/json"
     "github.com/goRESTapi/models"
     "net/http"
 )
@@ -11,15 +11,24 @@ import (
 var resp models.Response
 
 func ExceptionMessage(w http.ResponseWriter, message string, statusCode int){
-  generateOutput(w, message, statusCode)
+    generateOutput(w, message, statusCode)
 }
 
-//func JSONResponse(w http.ResponseWriter,user models.User){
 func JSONResponse(w http.ResponseWriter,i interface{}){
-    m := make(map[string]models.User)
-    m["data"] = user
 
-    json.NewEncoder(w).Encode(m)
+    switch v := i.(type) {
+    case models.User:
+        m := make(map[string]models.User)
+        m["data"] = v
+        json.NewEncoder(w).Encode(m)
+
+    case models.Post:
+        m := make(map[string]models.Post)
+        m["data"] = v
+        json.NewEncoder(w).Encode(m)
+    default:
+        fmt.Printf("Provided stryct type %T is not allowed \n", v)
+    }
 }
 
 func JSONListResponse(w http.ResponseWriter,res []models.User){
@@ -30,13 +39,13 @@ func JSONListResponse(w http.ResponseWriter,res []models.User){
 }
 
 func HttpResponse(w http.ResponseWriter, message string, statusCode int){
-  generateOutput(w, message, statusCode)
+    generateOutput(w, message, statusCode)
 }
 
 func generateOutput(w http.ResponseWriter, message string, statusCode int){
-  resp.Message=fmt.Sprintf(message)
-  resp.StatusCode=statusCode
-  m := make(map[string]models.Response)
-  m["data"] = resp
-  json.NewEncoder(w).Encode(m)
+    resp.Message=fmt.Sprintf(message)
+    resp.StatusCode=statusCode
+    m := make(map[string]models.Response)
+    m["data"] = resp
+    json.NewEncoder(w).Encode(m)
 }
